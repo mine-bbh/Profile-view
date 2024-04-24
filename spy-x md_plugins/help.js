bd2a6140f35b411691ba26810372f7804e899317
Danupa0 <161790821+Danupa0@users.noreply.github.com>
2024-04-24 08:10:47 -0700

    Update help.js

--- spy-x md_plugins/help.js
+++ spy-x md_plugins/help.js
@@ -4,22 +4,16 @@
 //                                                                                                      //
 //                                             𝚅.1.2.7                                                  // 
 //                                                                                                      //
-//                          ██╗ ██████╗ ██╗███████╗    ███╗   ███╗ ██████╗                              //
-//                          ██║ ██╔══██╗██║██╔════╝    ████╗ ████║ ██╔══██╗                             //
-//                          ██║ ██████╔╝██║███████╗    ██╔████╔██║ ██║  ██║                             //
-//                          ██║ ██╔══██╗██║╚════██║    ██║╚██╔╝██║ ██║  ██║                             //
-//                          ██║ ██║  ██║██║███████║    ██║ ╚═╝ ██║ ██████╔╝                             //
-//                          ╚═╝ ╚═╝  ╚═╝╚═╝╚══════╝    ╚═╝     ╚═╝ ╚═════╝                              //
 //                                                                                                      //
-//                                          BY: VENOX-SENPAI                                            //
+//                                          BY: DANUPA-X.z                                              //
 //                                                                                                      //
 //                                                                                                      //
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 
 /**
 
-* @project_name : IRIS-MD
-* @Developer : Venox-Senpai
+* @project_name : spy-x-md
+* @Developer : Danupa-Xz
 * @Version : v.1.2.7
 * @license : APACHE-2.0
 
@@ -29,10 +23,9 @@ TO A DMCA REPORT FROM THE OFFICIAL GITHUB, WHICH RESULT IN
 REMOVING UR COPIED/RE-UPLOADED PROJECT FROM THERE, IN SOME
 CONDITIONS IT CAN LEADS TO TERMINATE UR ACCOUNT FROM THAT PLATFORM
 
-│• @C_holder : Venox-Senpai
+│• @C_holder : Danupa-Xz
 
 **/
-
 
 
 
